0b9dc5cfdc544cc92e52f4636aeb9a34bf768093
Jadeilson Martinho <jadeilsonm17@gmail.com>
2022-06-11 13:25:17 -0300

    style: cria os buttons

--- App.tsx
+++ App.tsx
@@ -1,16 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [value, setValue] = useState('ERROR');
+
+  const buttons = ['AC','DEL','%','/','7','8','9','*','4','5','6','-','3','2','1','+','0','.','+/-','=',]
+  
+  const valueFinally = (valueButton: string) => {
+    setValue(value.concat(valueButton));
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.result}>
+        <Text style={styles.textResult}>{value}</Text>
+      </View>
+      <View style={styles.containerButton}>
+        {buttons.map(button =>
+          <TouchableOpacity 
+            key={button} 
+            style={styles.button}
+            onPress={() => valueFinally(button)}
+          >
+            <Text style={styles.text}>{button}</Text>
+          </TouchableOpacity>
+        )}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  result: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    width: '100%',
+    height: 350,
+    backgroundColor: '#f5f5f5'
+  },
+  textResult: {
+    fontSize: 26,
+    color: '#555555',
+    marginHorizontal: 12,
+  },
+  containerButton: {
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  button:{
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    minHeight: 95,
+    minWidth: 95
+  },
+  text:{
+    color: '#5b5b5b',
+    fontSize: 25
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
